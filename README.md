37a7be9c8139b15144fd731da5160ea67a30d98b
Joe Bacal <bacalj@gmail.com>
2025-05-04 19:18:02 -0400

    test env var

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 This is a collection of Netlify functions that are used as endpoints for the Access CI UI QA Bot.
 
-## Setup
-
 ## Deploying
+
+`git push origin main`
+
+## URL
+
+https://access-serverless-api.netlify.app/.netlify/functions/test-create-one
--- netlify/functions/test-create-one/test-create-one.ts
+++ netlify/functions/test-create-one/test-create-one.ts
@@ -1,12 +1,16 @@
 import { Handler } from '@netlify/functions'
 
+
 export const handler: Handler = async (event, context) => {
-  const { name = 'stranger' } = event.queryStringParameters
+  const localEnvVar = process.env.LOCAL_VAL
+  const { name = 'stranger' } = event.queryStringParameters || {}
+
+  console.log('| localEnvVar', localEnvVar)
 
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: `Hello, ${name}!`,
+      message: `Hello, ${name}! The local env var is ${localEnvVar}`,
     }),
   }
 }
