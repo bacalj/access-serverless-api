3f39d5ebd479d7ba2a6956646815187903ecf45a
Joe Bacal <bacalj@gmail.com>
2025-05-05 12:50:30 -0400

    cors headers

--- netlify/functions/test-create-one/test-create-one.ts
+++ netlify/functions/test-create-one/test-create-one.ts
@@ -1,7 +1,22 @@
 import { Handler } from '@netlify/functions'
 
-
 export const handler: Handler = async (event, context) => {
+  // Define CORS headers
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+  }
+
+  // Handle OPTIONS request (preflight request)
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 204,
+      headers,
+      body: ''
+    }
+  }
+
   const localEnvVar = process.env.LOCAL_VAL
   const { name = 'stranger' } = event.queryStringParameters || {}
 
@@ -10,6 +25,7 @@ export const handler: Handler = async (event, context) => {
 
   return {
     statusCode: 200,
+    headers,
     body: JSON.stringify({
       message: `Hello, ${name}! The local env var is ${localEnvVar}`,
     }),
